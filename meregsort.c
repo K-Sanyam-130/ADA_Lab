601aa132568484c9f1bd15fec3fecf9c6188a634
K Sanyam Khajuria <ksanyam.cs24@bmsce.ac.in>
2026-06-01 10:46:01 +0530

    First code

---
+++ meregsort.c
@@ -0,0 +1,67 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+void merge(int a[], int l, int m, int r){
+    int i = l, j = m + 1, k = 0;
+    int t[r - l + 1];
+
+    while(i <= m && j <= r){
+        t[k++] = (a[i] <= a[j]) ? a[i++] : a[j++];
+    }
+
+    while(i <= m){
+        t[k++] = a[i++];
+    }
+
+    while(j <= r){
+        t[k++] = a[j++];
+    }
+
+    for(i = l, k = 0; i <= r; i++, k++){
+        a[i] = t[k];
+    }
+}
+
+void mergesort(int a[], int l, int r){
+    if(l < r){
+        int m = (l + r) / 2;
+
+        mergesort(a, l, m);
+        mergesort(a, m + 1, r);
+
+        merge(a, l, m, r);
+    }
+}
+
+int main(){
+    int n;
+
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int *a = (int*)malloc(n * sizeof(int));
+
+    printf("Enter the elements:\n");
+    for(int i = 0; i < n; i++){
+        scanf("%d", &a[i]);
+    }
+
+    clock_t start = clock();
+
+    mergesort(a, 0, n - 1);
+
+    clock_t end = clock();
+
+    printf("Sorted Array:\n");
+    for(int i = 0; i < n; i++){
+        printf("%d ", a[i]);
+    }
+
+    double time_taken = (double)(end - start) / CLOCKS_PER_SEC;
+
+    printf("\nTime taken: %f seconds\n", time_taken);
+
+    free(a);
+    return 0;
+}
